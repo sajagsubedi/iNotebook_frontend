feab5542d30c836dad00f830a02bf325e6bb3880
Sajag Subedi <108583402+sajagsubedi@users.noreply.github.com>
2023-03-24 13:48:56 +0545

    Create index.jsx

---
+++ src/components/index.jsx
@@ -0,0 +1,21 @@
+import Header from "./Header.jsx";
+import Home from "./Home";
+import NoteComponent from "./NoteComponent";
+import ManipulateNote from "./ManipulateNote";
+import Alert from "./Alert";
+import About from "./About";
+import Login from "./Login";
+import Signup from "./Signup";
+import Spinner from "./Spinner";
+
+export {
+  Header,
+  Home,
+  NoteComponent,
+  ManipulateNote,
+  Spinner,
+  Alert,
+  About,
+  Signup,
+  Login,
+};
