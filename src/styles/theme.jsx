5abf63fe2564eea0bb3d0ef4e41a31a810ce2209
Sajag Subedi <108583402+sajagsubedi@users.noreply.github.com>
2023-03-24 13:50:23 +0545

    Create theme.jsx

---
+++ src/styles/theme.jsx
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    themecolor: "#0f0d1a",
+    secondaryTheme: "crimson",
+    blackShade: "rgba(0, 0, 0.2, 0.5)",
+    success: "#34D399",
+    error: "#EF4444",
+    sptxt: "#3B82F6",
+    bordershade: "#FECACA",
+    darkbordershade: "#F87171",
+    focus: "#ebecf0",
+  },
+  resposiveBreakPoints: {
+    mobile: "900px",
+  },
+};
