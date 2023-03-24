a53d3610a54a34caea67bf7248750a26c1b491df
Sajag Subedi <108583402+sajagsubedi@users.noreply.github.com>
2023-03-24 13:52:50 +0545

    Create index.jsx

---
+++ src/context/index.jsx
@@ -0,0 +1,19 @@
+import NoteContext from "./notes/NoteContext";
+import NoteState from "./notes/NoteState";
+import AuthContext from "./auth/AuthContext";
+import AuthState from "./auth/AuthState";
+import AlertState from "./alert/AlertState";
+import AlertContext from "./alert/AlertContext";
+import GlobalContext from "./global/GlobalContext";
+import GlobalState from "./global/GlobalState";
+
+export {
+  AlertContext,
+  NoteContext,
+  NoteState,
+  AuthState,
+  AuthContext,
+  AlertState,
+  GlobalState,
+  GlobalContext,
+};
